53433121d295e1378a107f6e90c80091a3e5f5ff
suu3 <ahob789@ewhain.net>
2022-10-04 07:15:26 +0900

    2강 - 반응형 그리드

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,22 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Input, Menu, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>노드버드</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}></Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="#" target="_blank" rel="noreferrer noopener">
+            Made by Suu3
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
-  return <div>프로필 페이지</div>;
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>프로필 페이지</div>
+      </AppLayout>
+    </>
+  );
 };
 
 export default Profile;
